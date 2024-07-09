5b0bd2e26089d711d6e8d99d9d0225fe30ebce53
plan1xaa <irubtsov05@icloud.com>
2024-07-09 23:14:30 +0300

    Added Btmp.h

--- Firmware/Arduino/PWMfrequency/PWMfrequency.ino
+++ Firmware/Arduino/PWMfrequency/PWMfrequency.ino
@@ -2,17 +2,20 @@
 
 void setup() 
 {
+Serial.begin(115200);
 
-pinMode(3, OUTPUT);
+//pinMode(3, OUTPUT);
 
-TCCR2B = (TCCR2B & 0xF8) | 0x01;
+//TCCR2B = (TCCR2B & 0xF8) | 0x01;
 
 
 }
 
 void loop() 
 {
-
-analogWrite(3, 150);
+if(Serial.available() > 0)
+{
+  Serial.println(Serial.read());
+}
 
 }
